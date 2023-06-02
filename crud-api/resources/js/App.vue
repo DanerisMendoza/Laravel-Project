f8237700187b6a667c076c51b49d5d893c6d6d25
Daneris <danerismendoza096@gmail.com>
2023-06-02 15:07:47 +0800

    fetching of data to table with bootstrap

--- crud-api/resources/js/App.vue
+++ crud-api/resources/js/App.vue
@@ -1,50 +1,18 @@
-<template>
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <div>Title: {{title}}</div>
-    <input type="text" ref="name">
-    <button @click="alertInput">Alert Your Input</button>
-    <button @click="clear">Clear</button>
-    <br><br>
+  <template>
     <RegistrationComp/>
+    <studentsTableComp/>
   </template>
   
   <script>
-  //this is only need when im importing HelloWorld component
-  // import HelloWorld from './components/HelloWorld.vue'
-//   import RegistrationComp from './components/RegistrationComp';
-  
+  import 'bootstrap/dist/css/bootstrap.css';
+
+  import RegistrationComp from './components/RegistrationComp.vue';
+  import studentsTableComp from './components/studentsTableComp.vue';
+
   export default {
-    // name: 'App',
-    // components: {
-    //   HelloWorld
-    // }
     name: 'App',
-    // components: {RegistrationComp},
-    data(){
-      return{
-        title: 'The litle Giant',
-        author: 'Bulfo Macaren',
-        age: 25,
-        condition: true,
-        books: [
-            {title: 'Unconditional love', author: 'kim soo', isFave: true},
-            {title: 'love the way you are', author: 'pawarit', isFave: false},
-            {title: 'life is hard', author: 'vincent lancough', isFave: true},
-        ]
-      }
-    },
-    methods: {
-      alertInput(){
-        alert(this.$refs.name.value);
-        console.log(this.$refs.name);
-        this.$refs.name.classList.add('active');
-        this.$refs.name.focus();
-      },
-      clear(){
-        this.$refs.name.value = '';
-      }
-    }
+    components: {RegistrationComp, studentsTableComp},
+
   }
   </script>
   
---
+++ crud-api/resources/js/components/studentsTableComp.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+    <h1>Student List</h1>
+    <table class="table table-bordered table-striped">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>created_at</th>
+            <th>updated_at</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="student in students" :key="student.id">
+            <td>{{ student.id }}</td>
+            <td>{{ student.fname }}</td>
+            <td>{{ student.lname }}</td>
+            <td>{{ student.email }}</td>
+            <td>{{ student.password }}</td>
+            <td>{{ student.created_at }}</td>
+            <td>{{ student.updated_at }}</td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    // variables
+    data() {
+      return {
+        students: [],
+      };
+    },
+    // create() is called when a component is created or initialized. then it will trigger the fetchStudents() method
+    created() {
+      this.fetchStudents();
+    },
+    // methods then it's getting data to db then pasting it to students array
+    methods: {
+      fetchStudents() {
+        axios
+          .get('/api/viewStudent')
+          .then(response => {
+            this.students = response.data;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      },
+    },
+  };
+  </script>
+  
